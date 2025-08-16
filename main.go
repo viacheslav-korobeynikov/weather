13208142ee04a7b33b12fb34d1a2c89c1886ec3f
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-16 06:20:37 +0300

    Добавлен Reader

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+)
 
+func main() {
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	fmt.Println(*format)
+
+	// Создали Reader
+	data := strings.NewReader("Привет! Я поток данных")
+	// Блок потока данных из Reader
+	byteData := make([]byte, 4)
+	for {
+		_, err := data.Read(byteData)
+		fmt.Printf("%q", byteData)
+		// Проверка на то, что файл закончился
+		if err == io.EOF {
+			break
+		}
+	}
 }
